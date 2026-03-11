a5ac96d363066cf78ffefdf7ac2238f7b179a07b
NDevTK <ndevtk@protonmail.com>
2026-03-11 02:43:07 +0000

    Update build

--- dist/worker.bundle.js
+++ dist/worker.bundle.js
@@ -46459,16 +46459,22 @@ ${rootStack}`;
     }
     if ((isCalleeIdentifier(node, "setTimeout", env) || isCalleeIdentifier(node, "setInterval", env) || isCalleeIdentifier(node, "queueMicrotask", env) || isCalleeIdentifier(node, "requestAnimationFrame", env)) && node.arguments[0]) {
       let callback = node.arguments[0];
+      const origCallbackName = callback.type === "Identifier" ? callback.name : null;
       if (callback.type === "Identifier") {
         const refKey = resolveId(callback, ctx);
         callback = ctx.funcMap.get(refKey) || ctx.funcMap.get(callback.name) || callback;
       }
       if (callback.type === "ArrowFunctionExpression" || callback.type === "FunctionExpression" || callback.type === "FunctionDeclaration") {
-        const childEnv = (callback._closureEnv || env).child();
-        if (callback.body.type === "BlockStatement") {
-          analyzeInlineFunction(callback, childEnv, ctx);
+        const cbKey = origCallbackName ? `timer:${origCallbackName}` : callback.loc ? `timer:${callback.loc.start.line}:${callback.loc.start.column}` : null;
+        if (cbKey && ctx.analyzedCalls.has(cbKey)) {
         } else {
-          evaluateExpr(callback.body, childEnv, ctx);
+          if (cbKey) ctx.analyzedCalls.set(cbKey, TaintSet.empty());
+          const childEnv = (callback._closureEnv || env).child();
+          if (callback.body.type === "BlockStatement") {
+            analyzeInlineFunction(callback, childEnv, ctx);
+          } else {
+            evaluateExpr(callback.body, childEnv, ctx);
+          }
         }
       }
     }
